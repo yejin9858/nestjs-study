e60240a07effc9f8fd98af1d3b245af7a5c0fe0c
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-13 01:56:27 +0900

    [FEAT] id로 특정 게시물 찾기 api

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -13,8 +14,14 @@ export class BoardsController {
 
     @Post()
     createBoard(
-        @Body('title') title: string, 
-        @Body('description') description: string) : Board{
-            return this.boardsService.createBoard(title, description);
+        @Body() createBoardDto:CreateBoardDto) : Board{
+            return this.boardsService.createBoard(createBoardDto);
     }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id:String) {
+        return this.boardsService.getBoardById(id);
+    }
+
+
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import {v1 as uuid} from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,7 +11,8 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title : string, description : string){
+    createBoard(createBoardDto : CreateBoardDto){
+        const{title, description} = createBoardDto;
         const board : Board= {
             id : uuid,
             title,
@@ -20,5 +22,9 @@ export class BoardsService {
 
         this.boards.push(board);
         return board;
+    }
+
+    getBoardById(id : String) : Board {
+        return this.boards.find((board) => board.id === id)
     }
 }
